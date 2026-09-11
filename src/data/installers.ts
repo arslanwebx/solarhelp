91c4533888cab7f3ecd482c6f4066fd1db7e07d5
Arslan Ejaz <arslanrajputseo@gmail.com>
2026-09-12 04:46:29 +0500

    Add installer coverage data

---
+++ src/data/installers.ts
@@ -0,0 +1,50 @@
+export const INSTALLERS = [
+  {
+    name: 'OWE',
+    states: ['AZ', 'CA', 'CO', 'FL', 'IL', 'MA', 'MD', 'ME', 'NH', 'NJ', 'NM', 'NY', 'PR', 'RI', 'TX', 'VA'],
+    stateNotes: {}
+  },
+  {
+    name: 'UNIVERSAL SOLAR',
+    states: ['AZ', 'IL', 'NM', 'OH', 'IN'],
+    stateNotes: { IL: 'ON HOLD' }
+  },
+  {
+    name: 'ZEO ENERGY',
+    states: ['OH', 'FL', 'VA', 'PA'],
+    stateNotes: {}
+  },
+  {
+    name: 'EMPOWER (NEW)',
+    states: ['AR', 'AZ', 'CA', 'CO', 'CT', 'FL', 'GA', 'IA', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MO', 'NC', 'NH', 'NJ', 'NY', 'OH', 'OK', 'PA', 'RI', 'SC', 'TX', 'VA', 'VT', 'WA'],
+    stateNotes: {}
+  },
+  {
+    name: 'ACTION SOLAR',
+    states: ['AZ', 'CA', 'CO', 'FL', 'GA', 'ID', 'MA', 'NV', 'NJ', 'NM', 'TX', 'UT', 'WI', 'WY'],
+    stateNotes: {}
+  },
+  {
+    name: 'SUNVENA',
+    states: ['FL', 'GA'],
+    stateNotes: {}
+  },
+  {
+    name: 'SUNDER (SUNPOWER/AMBIA)',
+    states: ['CA', 'CO', 'CT', 'FL', 'ID', 'IL', 'IN', 'MD', 'MA', 'MI', 'NV', 'NC', 'OH', 'OR', 'PA', 'UT', 'VA', 'WA'],
+    stateNotes: {}
+  },
+  {
+    name: 'LUNEX SOLAR',
+    states: ['CO', 'CT', 'FL', 'MA', 'RI'],
+    stateNotes: {}
+  },
+  {
+    name: 'TRON SOLAR',
+    states: ['IL', 'IN', 'WI'],
+    stateNotes: {}
+  }
+];
+
+// Lender coverage will be added here when lender data is provided.
+export const LENDERS = [];
